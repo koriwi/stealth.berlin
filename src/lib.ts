3ecddef650eae812f07e0eb02f84e2030f418a95
Kilian Gosewisch <kilian2798@gmail.com>
2023-03-22 19:10:55 +0100

    refactor

--- src/lib.ts
+++ src/lib.ts
@@ -1,3 +1,9 @@
 export const iff = (condition: boolean, then: any, otherwise: any) => {
   return condition ? then : otherwise;
 };
+
+export const cleanName = (name: string) => {
+  const noUnderscore = iff(name.startsWith("_"), name.slice(1), name);
+  const cleanText = noUnderscore[0].toUpperCase() + noUnderscore.slice(1);
+  return cleanText;
+};
