aa290e579e77d757885776ef2c25d6cb66e3cd44
Kilian Gosewisch <kilian2798@gmail.com>
2023-03-26 14:41:38 +0200

    wip; only dev works

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,13 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
+import dynamicImport from "vite-plugin-dynamic-import";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://stealth.berlin",
-
+  vite: {
+    plugins: [dynamicImport({})],
+  },
   integrations: [mdx(), sitemap(), tailwind(), image()],
 });
