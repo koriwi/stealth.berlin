297988b74c93f576ec9a32a4d4d11e4e0cd64439
Kilian Gosewisch <kilian2798@gmail.com>
2024-01-27 21:59:45 +0100

    feat: upgrades astro version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,11 @@
-import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
-import dynamicImport from "vite-plugin-dynamic-import";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://stealth.berlin",
 
-  integrations: [
-    mdx(),
-    sitemap(),
-    tailwind(),
-    image({ serviceEntryPoint: "@astrojs/image/sharp" }),
-  ],
+  integrations: [mdx(), sitemap(), tailwind()],
 });
